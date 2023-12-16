51a1eb7ba94606b1d927b9998a9dd037e33774ef
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-12-16 20:49:58 -0300

    (tasks/kaggle/main.py): Apply xcom to receive a value from previous task

--- tasks/kaggle/main.py
+++ tasks/kaggle/main.py
@@ -2,24 +2,11 @@ import os
 import logging
 from pymongo import MongoClient
 
-def download_csv(kaggle_credentials, mongo_credentials):
-    os.environ["KAGGLE_USERNAME"] = kaggle_credentials["kaggle_user"]
-    os.environ["KAGGLE_KEY"] = kaggle_credentials["kaggle_password"]
-    from kaggle.api.kaggle_api_extended import KaggleApi
 
-    try:
-        api = KaggleApi()
-        api.authenticate()
-    except Exception as e:
-        raise Exception(f"An error: {e}")
-
-    try:
-        connection_config = f"mongodb://{mongo_credentials['user']}:{mongo_credentials['password']}@{mongo_credentials['host']}:{mongo_credentials['port']}/"
-        client = MongoClient(connection_config)
-        db_connection = client[mongo_credentials["schema"]]
-        collection = db_connection["initialize_airflow"]
-        for i in collection.find({}):
-            print(f"Estou aqui: {i}")
-        print(f"It works: {db_connection}")
-    except Exception as e:
-        raise Exception(f"An error: {e}")
+def download_csv(kaggle_credentials, mongo_credentials, **kwargs):
+    print(kaggle_credentials["kaggle_user"])
+    print(kaggle_credentials["kaggle_password"])
+    print(mongo_credentials)
+    dataset = kwargs["ti"]
+    dataset = dataset.xcom_pull(task_ids="get_dataset_to_download", key="dataset")
+    print(dataset)
