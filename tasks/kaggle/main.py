24ee47a1819e5253a1ace83a0effacc3703ac234
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-12-20 16:43:37 -0300

    (tasks/kaggle/main.py): Add logic to download the dataset and retrieve the dataset file name to be used in the next task

--- tasks/kaggle/main.py
+++ tasks/kaggle/main.py
@@ -2,7 +2,21 @@ import os
 import logging
 
 
-def download_dataset(kaggle_credentials, mongo_credentials, **kwargs):
+def download_dataset(kaggle_credentials: dict, **kwargs) -> bool:
+    """Receive the dataset info from previous task and realize the download of dataset
+    from kaggle.
+
+    Args:
+        kaggle_credentials (dict): A dict with the kaggle credentials.
+        Ex: {"kaggle_user": "myuser", "kaggle_password": "mypassword"}
+
+    Raises:
+        Exception: The first exception is raised in authentication error.
+        Exception: The second exception is raised in data set download error.
+
+    Returns:
+        bool: True if no exception was raised.
+    """
     os.environ["KAGGLE_USERNAME"] = kaggle_credentials["kaggle_user"]
     os.environ["KAGGLE_KEY"] = kaggle_credentials["kaggle_password"]
     logging.info(f"The kaggle credentials: {kaggle_credentials}")
@@ -25,11 +39,17 @@ def download_dataset(kaggle_credentials, mongo_credentials, **kwargs):
         )
         logging.info(f"The dataset in list: {list_of_datasets}")
         for dataset in list_of_datasets:
+            print(dataset)
             api.dataset_download_files(
                 dataset=dataset.ref, path=".", force=False, unzip=True
             )
-            logging.info("The dataset has been downloaded successfully!")
-            logging.info(f"Files in current location: {os.listdir()}")
+
+        list_of_csv = [file for file in os.listdir() if ".csv" in file]
+        kwargs["ti"].xcom_push(key="dataset", value=list_of_csv)
+        # use xcom_push to pass list_of_csv to next task
+        logging.info(f"Here is the list of downloaded csv: {list_of_csv}")
+        logging.info("The dataset has been downloaded successfully!")
+        logging.info(f"Files in current location: {os.listdir()}")
     except Exception as e:
         raise Exception(f"An error: {e}")
     return True
--- tasks/upload_csv_to_storage/main.py
+++ tasks/upload_csv_to_storage/main.py
@@ -2,4 +2,6 @@ from tasks.utils.gcp_wrapper.main import GCPWrapper
 import os
 
 
-# def
+def upload_blob_file_to_bucket(**kwargs):
+    print(kwargs["ti"].xcom_pull(task_ids="getting_dataset", key="dataset"))
+    return True
