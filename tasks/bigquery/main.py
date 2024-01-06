b0f8160e9576513994ad8710607055792bf6f47a
DadosComCafe <dadoscomcafe.dev@gmail.com>
2024-01-05 21:23:56 -0300

    (tasks/bigquery/main.py): Initialize the workflow to make this ingestion

--- tasks/bigquery/main.py
+++ tasks/bigquery/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import pandas as pd
 from psycopg2 import connect
 from google.cloud import bigquery
 
@@ -8,7 +9,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/airflow/credentials/credentials
 
 def realize_get_in_postgres(
     postgres_credentials: dict, list_from_csvs_path: list
-) -> list:
+) -> tuple:
     table_name = list_from_csvs_path[0].replace(".csv", "").lower()
     query = f"SELECT * FROM {table_name}"
     try:
@@ -24,7 +25,15 @@ def realize_get_in_postgres(
         raise Exception(f"An error: {e}")
     cur.execute(query)
     data = cur.fetchall()
-    return data
+    cols = [col[0] for col in cur.description]
+    return data, cols
+
+
+def get_dataframe(fetchall_values: tuple) -> pd.DataFrame:
+    records = fetchall_values[0]
+    cols = fetchall_values[1]
+    df = pd.DataFrame(records, columns=cols)
+    return df
 
 
 def run_bigquery(postgres_credentials: dict, **kwargs):
@@ -32,13 +41,14 @@ def run_bigquery(postgres_credentials: dict, **kwargs):
     list_from_csvs_path = list_from_csvs_path.xcom_pull(
         task_ids="upload_to_gcp", key="csvs_path"
     )
-    return_of_select_all = realize_get_in_postgres(
+    fetchall_values = realize_get_in_postgres(
         postgres_credentials=postgres_credentials,
         list_from_csvs_path=list_from_csvs_path,
     )
+    df_values = get_dataframe(fetchall_values)
     try:
         client = bigquery.Client()
-        logging.info(f"Aqui a query: {return_of_select_all[0]}")
+        logging.info(f"The query in dataframe: {df_values}")
         logging.info("BigQuery client has been initialized successfully!")
     except Exception as e:
         raise Exception(f"An error: {e}")
